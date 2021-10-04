38beca5f779916959f40c40c12d6e178bfd7f012
abdulhadielyan <91835430+abdulhadielyan@users.noreply.github.com>
2021-10-04 20:34:13 +0400

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,10 @@
+splitstring = "THis is a string and it has been \n split\n  over several \n lines"
+print(splitstring)
+
+Name= " This \t is a new \t program "
+print(Name)
+
+print("""hey striing
+thios fhksdhf""")
+
+
