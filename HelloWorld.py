97c32c44927525d49fe39cbece34ea694dba52be
abdulhadielyan <91835430+abdulhadielyan@users.noreply.github.com>
2021-10-04 19:39:30 +0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print(1 + 2)
+print(2 * 3)
+print()
+my_file = 1223345.08
+s = "12343434"
+b = 123
+
+print(my_file)
+print(b)
+print(type(s))
+
+print("the end", " or is it ? ", " Keep watching python ", 3)
+
+print('we can even include "quotes" in string')
+
+print("greeting" + " hello")
+print("greeting" + " " + "hello")
+
+name = input("Enter something string here ")
+print(name)
